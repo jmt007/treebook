e524965150220fd3e092cb42930e64c829b48cd2
John Tabor <johnmtabor@gmail.com>
2013-05-12 22:23:48 -0500

    Added user friendships and associated test.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
+
   test "a user should enter a first name" do 
   	user = User.new
   	assert !user.save
@@ -38,12 +42,24 @@ test "a user should have a profile name without spaces" do
   assert user.errors[:profile_name].include?("Must be formatted correctly.")
 end
 
-test "a user can have a correctly formatted profile name" do
-  user = User.new(first_name: 'john', last_name: 'tabor', email: 'info@taborsecurity.com')
+test "a user should have a correctly formatted profile name" do
+  user = User.new(first_name: 'john', last_name: 'tabor', email: 'info@taborsecurity_1.com')
   user.password = user.password_confirmation = 'asdsadsa'
 
   user.profile_name = 'jmt0076'
   assert user.valid?
 end
+
+test "that no error is raised when trying to access a friend list" do
+  assert_nothing_raised do
+    users(:john).friends
+  end
+ end
+
+test "that creating friendships on a user works" do
+  users(:john).friends << users(:mike)
+  users(:john).friends.reload
+  assert users(:john).friends.include?(users(:mike))
+ end
 end
   
